05d3e3b827940601eeca25c7135bdb4284ba58c3
Erzhan Azimzhanov <erjanazimjanov@gmail.com>
2024-03-20 14:24:06 +0600

    asd

--- src/main/java/system/crm/web/mappers/UserMapper.java
+++ src/main/java/system/crm/web/mappers/UserMapper.java
@@ -6,5 +6,6 @@ import system.crm.domain.entity.User;
 import system.crm.web.dto.UserDto;
 
 @Mapper(componentModel = "spring")
+// roma pedik
 public interface UserMapper extends Mappable <User, UserDto> {
 }
