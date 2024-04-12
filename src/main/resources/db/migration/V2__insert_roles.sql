c7c000b9c627540267cdf33b4072226805a285e9
Amantur Zholdoshov <azholdoshevv@gmail.com>
2024-04-12 17:02:05 +0600

    Add admin

--- src/main/resources/db/migration/V2__insert_roles.sql
+++ src/main/resources/db/migration/V2__insert_roles.sql
@@ -1,5 +1,7 @@
-INSERT INTO users (name, username, password, date_of_birth, phone_number)
-VALUES ('Admin', 'admin', 'admin', '1990-01-01', '+996771328181');
+INSERT INTO users (name, username, password, date_of_birth, phone_number, enabled, image_url, last_visit_date, status)
+VALUES ('Amantur', 'admin', '$2a$12$Wdd4qDYvNGbo/gF97LqqM.rYb7mr/V/EqSLkeqBPj0fb4S/unHS5y', '2004-09-09', '+996771328181', true, 'admin.jpg', CURRENT_TIMESTAMP, 'ACTIVE');
 
 INSERT INTO users_roles (user_id, role)
-VALUES ((SELECT id FROM users WHERE username = 'admin'), 'ROLE_ADMIN');
+SELECT id, 'ROLE_ADMIN'
+FROM users
+WHERE username = 'admin';
